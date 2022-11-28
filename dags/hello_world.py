d3454c37cec4841dcf85f4feb8090beb2f2997b0
Nicholas Elia <nichelia@nicheliamacbookpro.lan>
2022-11-28 17:10:33 +0000

    Switching from double to single quotes - None scheduler needs no quotes

--- dags/hello_world.py
+++ dags/hello_world.py
@@ -4,14 +4,14 @@ from airflow.operators.python import PythonOperator
 
 
 def hello_world():
-    return "Hello World"
+    return 'Hello World'
 
 
-dag = DAG("hello_world_dag",
-          description="Hello World DAG",
+dag = DAG('hello_world_dag',
+          description='Hello World DAG',
           start_date=datetime(2022, 11, 1),
-          schedule_interval="None",
+          schedule_interval=None,
           catchup=False)
 
-task = PythonOperator(task_id="hello_world_task", python_callable=hello_world, dag=dag)
+task = PythonOperator(task_id='hello_world_task', python_callable=hello_world, dag=dag)
 task
