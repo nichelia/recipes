fe1463cc211557aef6c22edb3a5c1056a613f393
nichelia <nich.elia@gmail.com>
2022-11-28 17:43:12 +0000

    Testing imports

--- dags/gousto_recipe.py
+++ dags/gousto_recipe.py
@@ -1,11 +1,24 @@
 from datetime import datetime
+from typing import List
+import re
+
 from airflow import DAG
+from airflow import AirflowException
 from airflow.operators.python import PythonOperator
+from markdownify import markdownify as md
+from recipe_scrapers import scrape_me
+from recipe_scrapers.goustojson import GoustoJson
+import parse_ingredients
 
 
 def get_recipe(**kwargs):
     dag_run_conf = kwargs["dag_run"].conf
-    return dag_run_conf["url"]
+    url = dag_run_conf.get("url", None)
+
+    if not url:
+        raise ValueError('Url of gousto recipe not provided')
+
+    return url
 
 
 dag = DAG(
